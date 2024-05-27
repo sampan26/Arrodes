2feb26d26f3c6440456d00cd2d59c43a61323f7f
Turbobot <turbobot@vercel.com>
2024-05-27 11:43:08 -0400

    feat(create-turbo): apply official-starter transform

--- Typescript React tutorial/create-typescript-demo/src/components/Ex.tsx
+++
@@ -1,3 +0,0 @@
-export const Ex = () => {
-    return <div></div>
-}
--- Typescript React tutorial/create-typescript-demo/src/components/Person.tsx
+++
@@ -1,5 +0,0 @@
-import { PersonProps } from './Person.types'
-
-export const Person = (props: PersonProps) => {
-    return <div>{props.name.first} {props.name.last}</div>
-}
