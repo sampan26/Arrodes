30790b92b98386fb67c5c305d902bc3ba64aa6fc
sampan26 <rvbh87m@gmail.com>
2024-06-08 18:52:08 -0400

    changed some prisma var=

--- app/routers.py
+++ app/routers.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
-from app.api import agents
+from app.api import agents, auth, users
 
 router = APIRouter()
 api_prefix = "/api/v1"
 
 router.include_router(agents.router, tags=["Agent"], prefix=api_prefix)
+router.include_router(auth.router, tags=["Auth"], prefix=api_prefix)
+router.include_router(users.router, tags=["User"], prefix=api_prefix)
+ 
