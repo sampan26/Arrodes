a881350457e7eba6950eb5bbeb6b22802bef33eb
sampan26 <rvbh87m@gmail.com>
2024-06-19 00:28:20 -0400

    fuck this fly

--- app/lib/auth/prisma.py
+++ app/lib/auth/prisma.py
@@ -33,7 +33,7 @@ def decodeJWT(token: str) -> dict:
         return None
 
 def encryptPassword(password: str) -> str:
-    return bcrypt.hashmap(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
 def validatePassword(password: str, encrypted:str) -> str:
     return bcrypt.checkpw(password.encode("utf-8"), encrypted.encode("ut-f8"))
