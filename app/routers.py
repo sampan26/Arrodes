779ffc5c3fd7314e3ed4d80a83ec449c563cf66a
sampan26 <rvbh87m@gmail.com>
2024-06-19 00:28:08 -0400

    fixed encrypt

--- app/routers.py
+++ app/routers.py
@@ -8,4 +8,3 @@ api_prefix = "/api/v1"
 router.include_router(agents.router, tags=["Agent"], prefix=api_prefix)
 router.include_router(auth.router, tags=["Auth"], prefix=api_prefix)
 router.include_router(users.router, tags=["User"], prefix=api_prefix)
- 
