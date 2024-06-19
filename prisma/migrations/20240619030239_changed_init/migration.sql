3c4eba9bbc73c81eba613a172534d2f27c4ba8be
sampan26 <rvbh87m@gmail.com>
2024-06-18 23:10:11 -0400

    changed migrations

---
+++ prisma/migrations/20240619030239_changed_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateEnum
+CREATE TYPE "AgentType" AS ENUM ('REACT', 'PLANSOLVE');
+
+-- CreateTable
+CREATE TABLE "Agent" (
+    "id" VARCHAR(255) NOT NULL,
+    "userId" VARCHAR(255) NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" "AgentType" NOT NULL DEFAULT 'REACT',
+
+    CONSTRAINT "Agent_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Agent" ADD CONSTRAINT "Agent_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
