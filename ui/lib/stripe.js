52ff132ec9202082bf2f69e5c2fa549f464a9dcc
sampan26 <rvbh87m@gmail.com>
2024-07-23 23:11:56 -0400

    stripe auth

--- ui/lib/stripe.js
+++ ui/lib/stripe.js
@@ -1,3 +1,3 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+export const stripe = new Stripe("sk_test_51PeoFNAGJyXf68B96t5Ms06WtIFmaqIRO77qZYSoXN4w0fc8yJljqIIkboEr1TyPCCc8WURBxlvwlLAJKH4KpeCg00bMtcNOee");
