36d0a06f4fb11b8895dfd2b8d02f1ffea8652bf3
sampan26 <rvbh87m@gmail.com>
2024-07-29 23:54:30 -0400

    add tags

--- app/routers.py
+++ app/routers.py
@@ -8,6 +8,7 @@ from app.api import (
     auth,
     documents,
     prompts,
+    tags,
     tools,
     traces,
     users,
@@ -26,5 +27,6 @@ router.include_router(users.router, tags=["User"], prefix=api_prefix)
 router.include_router(api_tokens.router, tags=["Api token"], prefix=api_prefix)
 router.include_router(documents.router, tags=["Documents"], prefix=api_prefix)
 router.include_router(prompts.router, tags=["Prompts"], prefix=api_prefix)
+router.include_router(tags.router, tags=["Tags"], prefix=api_prefix)
 router.include_router(tools.router, tags=["Tools"], prefix=api_prefix)
 router.include_router(traces.router, tags=["Traces"], prefix=api_prefix)
